462a6a7ed07df02587d8d7375498f3b7eba3037c
sAtIjASS <priyanshuitsatija@gmail.com>
2024-12-05 17:35:54 +0530

    Hii It is My Flutter Internship work

--- lib/Todo_Application/screen/Show_Task_From_Database.dart
+++ lib/Todo_Application/screen/Show_Task_From_Database.dart
@@ -4,7 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+//This is for Showing the List of the Tasks
 RxList<dynamic> taskList = [].obs;
+
+
 
 //Show Task Part from Database
 Widget showTaskpart() {
