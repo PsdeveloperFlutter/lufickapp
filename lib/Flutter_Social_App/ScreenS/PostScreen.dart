e7146f8f789281f767d8f3a14450e4768083efc8
sAtIjASS <priyanshuitsatija@gmail.com>
2024-12-20 16:20:27 +0530

    Hii It is My Flutter Internship work

--- lib/Flutter_Social_App/ScreenS/PostScreen.dart
+++ lib/Flutter_Social_App/ScreenS/PostScreen.dart
@@ -5,16 +5,7 @@ import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:lufickapp/Flutter_Social_App/ScreenS/login_Page.dart';
 
-RxList<dynamic> imagelist = [
-  "assets/images/nature1..jpg",
-  "assets/images/nature2.jpg",
-  "assets/images/nature3.jpg",
-  "assets/images/nature4.jpg",
-].obs;
 
-RxList<int> likeanddislike = RxList<int>();
-RxList<bool> isPostVisible = RxList<bool>();
-RxList<String> postTimes = RxList<String>();
 
 class PostScreen extends StatefulWidget {
   @override
@@ -26,6 +17,7 @@ class _PostScreenState extends State<PostScreen> {
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -40,35 +32,95 @@ class _PostScreenState extends State<PostScreen> {
           onTap: () async {
             await GoogleSignIn().signOut();
             FirebaseAuth.instance.signOut();
-            Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPages()));
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => LoginPages()));
           },
           child: Text('Post'),
         ),
       ),
-      body: StreamBuilder(stream:FirebaseFirestore.instance.collection("users").snapshots() ,
-
-          builder: (context,index){
-
-            if(index.hasError){
-              return Center(child: Text("Error Occur :- ${index.error}"));
-            }
-
-            else if(!index.hasData || index.data!.docs.isEmpty){
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection("users").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text("Error Occur :- ${snapshot.error}"));
+            } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
               return Center(child: Text("No data found"));
             }
 
+            // Extract Firestore data
+            final users = snapshot.data!.docs;
 
+            return Card(
+              elevation: 7,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ListView.separated(
+                  itemCount:snapshot.data!.docs.length,
+                  itemBuilder: (context,index){
+                  return Card(
+                    elevation: 5,
+                    child:Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.cyanAccent.shade100,
+                                radius: 30,
+                                child: Text(users[index]['name'][0].toString(),style: TextStyle(color: Colors.black),),
+                              ),
+                              Text(users[index]['name'].toString()),
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Text("Post", style: TextStyle(decoration: TextDecoration.underline, fontWeight: FontWeight.bold),),
+                          SizedBox(height: 4,),
+                          Text(users[index]['post'][index]['posts'].toString()),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+                           crossAxisAlignment: CrossAxisAlignment.center,
+                           children: [
+                             Column(
+                               children: [
+                                 IconButton(onPressed: (){}, icon: Icon(Icons.thumb_up,color: Colors.red,)),
+                                 Text(users[index]['post'][index]['like'].toString()),
+                               ],
+                             ),
+                             Column(
+                               children: [
+                                 IconButton(onPressed: (){}, icon: Icon(Icons.thumb_down,color: Colors.blue,)),
+                                 Text(users[index]['post'][index]['Dislike'].toString()),
+                               ],
+                             )
+                             ,
+                             Column(
+                               children: [
+                                 IconButton(onPressed: (){}, icon: Icon(Icons.thumb_down,color: Colors.blue,)),
+                                 Text(users[index]['post'][index]['Dislike'].toString()),
+                               ],
+                             )
+                           ],
+                         )
 
-            //This is the User Post List Of all Person Which Is Available on the App
-            else {
-              return ListView.builder(
+                        ],
+                      ),),
+                    )
+                  );
 
-              )
-
-
-
-            }
-      })
+                  }, separatorBuilder: (BuildContext context, int index) {
+                    return Divider();
+                },
+                ),
+              ),
+            );
+          }),
     );
   }
 }
