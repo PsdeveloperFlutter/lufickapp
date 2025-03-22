106a2cb64f7f11812cd1f424e8f61e94ff181ca2
Priyanshu Satija <Nevergiveupjsr@gmail.com>
2025-03-22 16:05:29 +0530

    Update flutter_speed_dial to version 6.0.0 and implement color and tap functionalities.

--- lib/Animation_in_Flutter/Fab_Button.dart
+++ lib/Animation_in_Flutter/Fab_Button.dart
@@ -11,30 +11,49 @@ class _SimpleFabState extends State<SimpleFab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FloatingActionButton(
-        onPressed: () {},
-        child:SpeedDial(
-          animatedIcon: AnimatedIcons.menu_close,
-          children: [
-            SpeedDialChild(
-              child: Icon(Icons.add),
-              label: "Add"
-            ),
-            SpeedDialChild(
-                child: Icon(Icons.call),
-                label: "Call"
-            ),
-            SpeedDialChild(
-                child: Icon(Icons.access_alarms),
-                label: "Alarm"
-            ),
-            SpeedDialChild(
-                child: Icon(Icons.menu),
-                label: "Menu"
-            ),
-          ],
-        )
-      ),
+     floatingActionButton: FloatingActionButton(
+         onPressed: () {},
+         child:SpeedDial(
+           overlayColor: Colors.blue.shade500,
+           overlayOpacity:0.4,
+           animatedIcon: AnimatedIcons.menu_close,
+           children: [
+             SpeedDialChild(
+                 backgroundColor: Colors.yellow,
+                 child: Icon(Icons.add),
+                 label: "Add"
+                 ,onTap: (){
+               print("Priyanshu satija1");
+             }
+             ),
+             SpeedDialChild(
+                 backgroundColor: Colors.blue,
+                 child: Icon(Icons.call),
+                 label: "Call"
+                 ,onTap: (){
+               print("Priyanshu satija2");
+             }
+             ),
+             SpeedDialChild(
+                 backgroundColor: Colors.greenAccent,
+                 child: Icon(Icons.access_alarms),
+                 label: "Alarm"
+                 ,onTap: (){
+               print("Priyanshu satija3");
+             }
+             ),
+             SpeedDialChild(
+                 backgroundColor: Colors.green,
+                 child: Icon(Icons.menu),
+                 label: "Menu"
+                 ,onTap: (){
+               print("Priyanshu satija4");
+             }
+             ),
+           ],
+         )
+     ),
+      body: Center(child: Text("Priyanshusatija"))
     );
   }
 }
